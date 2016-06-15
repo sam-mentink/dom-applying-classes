7a0e27133b69d8f82be055c09541d3a3e89a4f67
sam.mentink <samari.b.dj@gmail.com>
2016-06-15 15:03:09 +1200

    5.3 sprint-5

--- classes.js
+++ classes.js
@@ -1,25 +1,34 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
+one();
+two();
+three();
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
-function one () {
+function one() {
   // First, we have to find the element:
   var one = document.getElementById('one')
 
   // Next, we apply a new CSS class to it:
-  one.classList.add('blue')
+  one.classList.add('green')
+
 }
-
 // CREATE FUNCTION two HERE
+function two() {
+  var two = document.getElementById('two')
+  two.classList.add('green')
 
+}
 // CREATE FUNCTION three HERE
+function three() {
+  var three = document.getElementById('three')
+  three.classList.add('purple')
 
+}
 // CREATE FUNCTION makeVisible HERE
